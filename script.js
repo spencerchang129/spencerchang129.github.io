38e5326c64295c3cbdb4ab9d9069146589b27267
Spencer <95193140+spencerchang129@users.noreply.github.com>
2023-08-04 03:29:09 -0400

    removed new tab

--- script.js
+++ script.js
@@ -5,19 +5,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-function newTabLinkedIn() {
-  window.open(
-    "https://www.linkedin.com/in/chang-spencer/",
-    "_blank",
-    "noopener noreferrer"
-  );
-}
-
-function newTabGitHub() {
-  window.open(
-    "https://github.com/spencerchang129?tab=repositories",
-    "_blank",
-    "noopener noreferrer"
-  );
-}
