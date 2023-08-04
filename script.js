4993ea219dac9366462f6e853beb6f83ae9ff494
Spencer <95193140+spencerchang129@users.noreply.github.com>
2023-08-04 02:58:55 -0400

    created about me

---
+++ script.js
@@ -0,0 +1,7 @@
+/* target elements menu-links and hamburger-icon and add or remove the open class*/
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
